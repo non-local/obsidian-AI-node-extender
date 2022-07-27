6c070d91dd5ee287514dc57f1a7bb5d1d28c78fa
non-local <133554321+non-local@users.noreply.github.com>
2022-07-27 15:50:58 +0000

    CellML Text view: some work on writing a parser for the CellML text format (#531) [ci skip].

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -20,3 +20,30 @@ const context = await esbuild.context({
 	external: [
 		"obsidian",
 		"electron",
+		"@codemirror/autocomplete",
+		"@codemirror/collab",
+		"@codemirror/commands",
+		"@codemirror/language",
+		"@codemirror/lint",
+		"@codemirror/search",
+		"@codemirror/state",
+		"@codemirror/view",
+		"@lezer/common",
+		"@lezer/highlight",
+		"@lezer/lr",
+		...builtins,
+	],
+	format: "cjs",
+	target: "es2018",
+	logLevel: "info",
+	sourcemap: prod ? false : "inline",
+	treeShaking: true,
+	outfile: "main.js",
+});
+
+if (prod) {
+	await context.rebuild();
+	process.exit(0);
+} else {
+	await context.watch();
+}
