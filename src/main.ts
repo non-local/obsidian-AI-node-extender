8546481e998ca92494f9949425dcfea8d1885770
non-local <133554321+non-local@users.noreply.github.com>
2023-01-30 11:48:55 +0000

    IB/mthca: Return an error on ib_copy_to_udata() failure

--- src/main.ts
+++ src/main.ts
@@ -32,4 +32,5 @@ export default class CanvasLLMExtendPlugin extends Plugin {
         // Add a new menu item to the canvas node menu
         this.registerEvent(this.app.workspace.on("canvas:node-menu", (menu: Menu, node: unknown) => {
                 menu.addItem((i: MenuItem) => {
-                    i.setSection("canvasLLMExt
+                    i.setSection("canvasLLMExtend");
+                    i.setTitl
