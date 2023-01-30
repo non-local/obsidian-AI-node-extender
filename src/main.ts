130eebdef5b4c3f7d14a33ac038580e6fb6e09d9
non-local <133554321+non-local@users.noreply.github.com>
2023-01-30 09:05:42 +0000

    Fixed the unit tests

--- src/main.ts
+++ src/main.ts
@@ -32,4 +32,4 @@ export default class CanvasLLMExtendPlugin extends Plugin {
         // Add a new menu item to the canvas node menu
         this.registerEvent(this.app.workspace.on("canvas:node-menu", (menu: Menu, node: unknown) => {
                 menu.addItem((i: MenuItem) => {
-     
+                    i.setSection("canvasLLMExt
