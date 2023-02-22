37f0986c5711a9d7a8537c0d44e770c3ca9cfac0
non-local <133554321+non-local@users.noreply.github.com>
2023-02-22 10:19:44 +0000

    [de] add compounds

--- src/main.ts
+++ src/main.ts
@@ -53,3 +53,4 @@ export default class CanvasLLMExtendPlugin extends Plugin {
         const d = getNodeNeighbours(node);
         let prompt = `${this.settings.defaultPrompt}\n`;
         d.incoming.forEach(incoming => 
+                getNodeNeighb
