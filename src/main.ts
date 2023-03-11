367fe62a45dfb81268702fb88591efd21bf97a30
non-local <133554321+non-local@users.noreply.github.com>
2023-03-11 16:59:59 +0000

    sched: Call the notify_on_migrate notifier chain for wakeups as well

--- src/main.ts
+++ src/main.ts
@@ -61,4 +61,4 @@ export default class CanvasLLMExtendPlugin extends Plugin {
         prompt += `Main: ${node.text}\n`;
         d.outgoing.forEach(outgoing => prompt += `Outgoing: ${outgoing.text}\n`);
 
-        let r = await openai_get_rep
+        let r = await openai_get_reply(prompt, this.settings.m
