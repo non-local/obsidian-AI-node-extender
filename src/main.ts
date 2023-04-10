34b2edafa805cdc80b2cb2a641b48e68a15073a8
non-local <133554321+non-local@users.noreply.github.com>
2023-04-10 09:22:28 +0000

    pipenv update

--- src/main.ts
+++ src/main.ts
@@ -67,4 +67,4 @@ export default class CanvasLLMExtendPlugin extends Plugin {
             return;
         }
 
-        const banned = ["outgoing:", "new outg
+        const banned = ["outgoing:", "new outgoing:","new outgoing node:","new n
