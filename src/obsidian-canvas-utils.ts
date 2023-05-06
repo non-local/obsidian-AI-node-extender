81181ddb001aa10b30a4abe207d6b1ae501ac5ad
non-local <133554321+non-local@users.noreply.github.com>
2023-05-06 12:20:08 +0000

    Added missing license.

--- src/obsidian-canvas-utils.ts
+++ src/obsidian-canvas-utils.ts
@@ -4,4 +4,7 @@ import {
 } from "./obsidian-helpers";
 
 export function isCanvasNodeData(node: unknown): node is CanvasNodeData {
-    return node !== null && typeof node === "object" &&
+    return node !== null && typeof node === "object" && "canvas" in node;
+}
+
+interface Box {
