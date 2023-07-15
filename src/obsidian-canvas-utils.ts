14726e4f09d123e77d6264f880a170e6326468de
non-local <133554321+non-local@users.noreply.github.com>
2023-07-15 21:26:15 +0000

    gcc fix

--- src/obsidian-canvas-utils.ts
+++ src/obsidian-canvas-utils.ts
@@ -77,4 +77,5 @@ function overlaps(node1: unknown, node2: unknown): boolean {
     return true;
 }
 // Attempts to place the node_to_fit at a location around neighbor_node
-// updown: attempt to p
+// updown: attempt to place above/below
+// leftright: attempt to plac
