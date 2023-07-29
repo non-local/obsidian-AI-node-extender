8088d7ccd9089f27fd0896cf9a68a00706a1388c
non-local <133554321+non-local@users.noreply.github.com>
2023-07-29 06:16:32 +0000

    Release version 0.0.3

--- src/obsidian-canvas-utils.ts
+++ src/obsidian-canvas-utils.ts
@@ -82,4 +82,4 @@ function overlaps(node1: unknown, node2: unknown): boolean {
 function findEmptySpace(neighbor_node: CanvasNodeData, node_to_fit: CanvasNodeData, distance_between: number, updown: boolean, leftright: boolean) {
     const positions = []
     if (leftright) {
-        positions.push({x: neighbor_node.x+neighbo
+        positions.push({x: neighbor_node.x+neighbor_node.width+distance_between, 
