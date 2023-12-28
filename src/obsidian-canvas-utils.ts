1e314f0298d272ecd4d8b03257b4145f3aa4552b
non-local <133554321+non-local@users.noreply.github.com>
2023-12-28 09:04:22 +0000

    remove unused scripts

--- src/obsidian-canvas-utils.ts
+++ src/obsidian-canvas-utils.ts
@@ -190,4 +190,5 @@ export function addNodeChild(node: CanvasNodeData, nodetext: string) {
     for (const o of altorder) {
         // TODO 15 is distance between nodes, make constant or setting
         if (findEmptySpace(o.node, n, 15, o.updown, o.leftright)) {
-            createEdge(no
+            createEdge(node, n, o.node, node.canvas)
+          
