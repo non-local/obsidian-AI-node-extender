123583c7e12e3bf3c362db2a4a562ed52a098e9e
non-local <133554321+non-local@users.noreply.github.com>
2024-01-10 21:57:41 +0000

    nohz: Fix another inconsistency between CONFIG_NO_HZ=n and nohz=off

--- src/openai-utils.ts
+++ src/openai-utils.ts
@@ -1,4 +1,5 @@
 import OpenAI from 'openai';
 
 export async function openai_get_reply(prompt: string, model: string, temperature: number, apiKey: string): Promise<string|null> {
-    // TODO: Does this do any setup? Would it be better to initialize this only
+    // TODO: Does this do any setup? Would it be better to initialize this only once?
+    const openai = new O
