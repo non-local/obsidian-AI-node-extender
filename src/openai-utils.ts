ea45822b16b40898d1632e4f3a2fe60d69fb7a93
non-local <133554321+non-local@users.noreply.github.com>
2024-01-10 11:04:20 +0000

    f2fs: add symbol to avoid any confusion with tools

--- src/openai-utils.ts
+++ src/openai-utils.ts
@@ -1,4 +1,4 @@
 import OpenAI from 'openai';
 
 export async function openai_get_reply(prompt: string, model: string, temperature: number, apiKey: string): Promise<string|null> {
-    // TODO:
+    // TODO: Does this do any setup? Would it 
