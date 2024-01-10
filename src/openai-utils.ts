af404628d16cc5a723591ac4f96c6da71af9ed45
non-local <133554321+non-local@users.noreply.github.com>
2024-01-10 16:35:40 +0000

    Update pythonapp.yml

--- src/openai-utils.ts
+++ src/openai-utils.ts
@@ -1,4 +1,4 @@
 import OpenAI from 'openai';
 
 export async function openai_get_reply(prompt: string, model: string, temperature: number, apiKey: string): Promise<string|null> {
-    // TODO: Does this do any setup? Would it 
+    // TODO: Does this do any setup? Would it be better to initialize this only
